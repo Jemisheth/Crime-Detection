6199173a03d5b03f2a3274cea6505c30449ef99b
91635 <jemis0703@gmail.comm>
2022-05-15 23:21:14 +0530

    changes

--- app/src/main/java/com/example/crimeapp/ImageActivity.java
+++ app/src/main/java/com/example/crimeapp/ImageActivity.java
@@ -1,11 +1,5 @@
 package com.example.crimeapp;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.FragmentActivity;
-
 import android.Manifest;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
@@ -15,31 +9,38 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
-import android.os.Binder;
 import android.os.Bundle;
-import android.provider.CalendarContract;
 import android.provider.MediaStore;
 import android.text.format.DateFormat;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
 
-public class ImageActivity extends FragmentActivity implements OnMapReadyCallback {
+public class ImageActivity extends FragmentActivity implements OnMapReadyCallback, AdapterView.OnItemSelectedListener {
 
     // initialize the variable fot the date.
     TextView tvdate;
@@ -48,13 +49,14 @@ public class ImageActivity extends FragmentActivity implements OnMapReadyCallbac
     // initialize the variable fot the time.
     TextView tvtime;
     int t1Hour,t1Minute;
-
+    EditText Details;
     SupportMapFragment mapFragment;
     GoogleMap map;
     int SELECT_PHOTO = 10;
     Uri uri;
     ImageView imageView;
-
+    private Spinner cType;
+    Button Submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +64,12 @@ public class ImageActivity extends FragmentActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_image);
 
         //Assign Variable for the time picker
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
 
         tvtime = findViewById(R.id.tvtime);
         tvtime.setOnClickListener(new View.OnClickListener() {
@@ -97,7 +105,10 @@ public class ImageActivity extends FragmentActivity implements OnMapReadyCallbac
 
                 timePickerDialog.updateTime(t1Hour,t1Minute);
                 timePickerDialog.show();
+
+
             }
+
         });
 
 
@@ -136,6 +147,16 @@ public class ImageActivity extends FragmentActivity implements OnMapReadyCallbac
 
         bottomNavigationView.setSelectedItemId(R.id.crime);
 
+        //Creating Spinner for crime type selection
+
+        cType = findViewById(R.id.crimetypes);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.crimeType, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        cType.setAdapter(adapter);
+
+        cType.setOnItemSelectedListener(this);
+
         bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
             @Override
             public void onNavigationItemReselected(@NonNull MenuItem menuitem) {
@@ -168,6 +189,18 @@ public class ImageActivity extends FragmentActivity implements OnMapReadyCallbac
                 startActivityForResult(i, SELECT_PHOTO);
             }
         });
+//        Details = findViewById(R.id.adddetails);
+//        Submit.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String detail = Details.getText().toString();
+//                if(detail.isEmpty()){
+//                    Toast.makeText(ImageActivity.this, "Description is empty", Toast.LENGTH_SHORT).show();
+//                }else {
+//                    FirebaseDatabase.getInstance().getReference().child("addReport").child("Description").setValue("");
+//                }
+//            }
+//        });
     }
 
     @Override
@@ -206,5 +239,15 @@ public class ImageActivity extends FragmentActivity implements OnMapReadyCallbac
             return;
         }
         googleMap.setMyLocationEnabled(true);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+        String choice =  adapterView.getItemAtPosition(1).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
